4e64c4df316c9e5ae8bad8d200882533448f7031
Diego M <diegomartell6654@gmail.com>
2026-01-27 14:32:51 -0600

    Practica-3-ALbum-2G

--- Practica-#3/Practica3/App.js
+++ Practica-#3/Practica3/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.TitleContainer}>
-        <Text>Proyect of a Person</Text>
+        <Text>Los 3 albumes que más escucho</Text>
       </View>
       <View style={styles.AlbumContainer}>
         <ScrollView horizontal={true}
@@ -16,17 +16,24 @@ export default function App() {
         >
           <View style={styles.ImageContainer}>
             <Image source={require('./assets/Album1.png')} style={styles.AlbumImage} />
+            <Text style={styles.TextAlbumContainer}>Al no poder elegir entre varios álbumes que me gustan, escogí los tres que más escucho y que son de
+              géneros distintos. El primero, Project of a Person de Lund, me gusta por el tipo de música (RAP) más que la letra,
+              la acústica es muy buena y creo que, en cuestión de velocidad, es bastante relajado.</Text>
           </View>
           <View style={styles.ImageContainer}>
             <Image source={require('./assets/Album2.png')} style={styles.AlbumImage} />
+            <Text style={styles.TextAlbumContainer}>El segundo es The Last Laugh de VOILA. Es Rock-Pop, un poco más enérgica y pongo un poco más de
+              atención a la letra. El álbum es relativamente reciente, o por lo menos una parte de él, y actualmente diría que es el que más escucho.
+            </Text>
           </View>
           <View style={styles.ImageContainer}>
             <Image source={require('./assets/Album3.png')} style={styles.AlbumImage} />
+            <Text style={styles.TextAlbumContainer}>El tercero es f.e.a.r de Stand Atlantic. Es un álbum de rock experimental, mucho más movido,
+              pero a su vez con cierto nivel de equilibrio entre la música y el ritmo de la letra. Me atrevería a decir que, de los tres,
+              es el que menos escucho.
+            </Text>
           </View>
         </ScrollView>
-      </View>
-      <View style={styles.TextAlbumContainer}>
-        <Text></Text>
       </View>
     </View>
 
@@ -46,29 +53,34 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 20,
   },
-  AlbumContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  TextAlbumContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   ImageContainer: {
     width: width,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  AlbumContainer: {
+    flex: 1,
+    marginTop: 20,
   },
   AlbumScrollView: {
     flexGrow: 1,
     height: 400,
   },
   AlbumImage: {
-    width: width * 0.8,
-    height: 300,
+    width: width * 0.85,
+    height: width * 0.85,
     backgroundColor: '#A2F5F0',
     borderRadius: 10,
+    padding: 10,
+  },
+  TextAlbumContainer: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: '25%',
+    marginHorizontal: '5%',
+    textAlign: 'center',
   },
 });
