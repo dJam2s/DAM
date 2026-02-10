12443fd5567aeb82590d366ef8841e9da77af889
Diego M <diegomartell6654@gmail.com>
2026-02-09 18:23:52 -0600

    Practica6-InstaLike-1G

--- Practica-#3/Practica3/App.js
+++ Practica-#3/Practica3/App.js
@@ -10,55 +10,60 @@ export default function App() {
     const offsetX = event.nativeEvent.contentOffset.x;
     const index = Math.round(offsetX / width);
     setCurrentIndex(index);
+
+    if (currentIndex !== pageIndex) {
+      setCurrentIndex(pageIndex);
+    }
   };
+};
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.TitleContainer}>
-        <Text>Los 3 albumes que más escucho</Text>
-      </View>
-      <View style={styles.AlbumContainer}>
-        <ScrollView horizontal={true}
-          showsHorizontalScrollIndicator={false}
-          pagingEnabled={true}
-          style={styles.AlbumScrollView}
-          onMomentumScrollEnd={handleScroll}
-        >
-          <View style={styles.ImageContainer}>
-            <Image source={require('./assets/Album1.png')} style={styles.AlbumImage} />
-            <Text style={styles.TextAlbumContainer}>Al no poder elegir entre varios álbumes que me gustan, escogí los tres que más escucho y que son de
-              géneros distintos. El primero, Project of a Person de Lund, me gusta por el tipo de música (RAP) más que la letra,
-              la acústica es muy buena y creo que, en cuestión de velocidad, es bastante relajado.</Text>
-          </View>
-          <View style={styles.ImageContainer}>
-            <Image source={require('./assets/Album2.png')} style={styles.AlbumImage} />
-            <Text style={styles.TextAlbumContainer}>El segundo es The Last Laugh de VOILA. Es Rock-Pop, un poco más enérgica y pongo un poco más de
-              atención a la letra. El álbum es relativamente reciente, o por lo menos una parte de él, y actualmente diría que es el que más escucho.
-            </Text>
-          </View>
-          <View style={styles.ImageContainer}>
-            <Image source={require('./assets/Album3.png')} style={styles.AlbumImage} />
-            <Text style={styles.TextAlbumContainer}>El tercero es f.e.a.r de Stand Atlantic. Es un álbum de rock experimental, mucho más movido,
-              pero a su vez con cierto nivel de equilibrio entre la música y el ritmo de la letra. Me atrevería a decir que, de los tres,
-              es el que menos escucho.
-            </Text>
-          </View>
-        </ScrollView>
-        <View style={styles.PaginationContainer}>
-          {[0, 1, 2].map((i) => (
-            <View
-              key={i}
-              style={[
-                styles.PaginationDot,
-                { opacity: currentIndex === i ? 1 : 0.3 }
-              ]}
-            />
-          ))}
+return (
+  <SafeAreaView style={styles.container}>
+    <View style={styles.TitleContainer}>
+      <Text>Los 3 albumes que más escucho</Text>
+    </View>
+    <View style={styles.AlbumContainer}>
+      <ScrollView horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        pagingEnabled={true}
+        style={styles.AlbumScrollView}
+        onMomentumScrollEnd={handleScroll}
+      >
+        <View style={styles.ImageContainer}>
+          <Image source={require('./assets/Album1.png')} style={styles.AlbumImage} />
+          <Text style={styles.TextAlbumContainer}>Al no poder elegir entre varios álbumes que me gustan, escogí los tres que más escucho y que son de
+            géneros distintos. El primero, Project of a Person de Lund, me gusta por el tipo de música (RAP) más que la letra,
+            la acústica es muy buena y creo que, en cuestión de velocidad, es bastante relajado.</Text>
         </View>
+        <View style={styles.ImageContainer}>
+          <Image source={require('./assets/Album2.png')} style={styles.AlbumImage} />
+          <Text style={styles.TextAlbumContainer}>El segundo es The Last Laugh de VOILA. Es Rock-Pop, un poco más enérgica y pongo un poco más de
+            atención a la letra. El álbum es relativamente reciente, o por lo menos una parte de él, y actualmente diría que es el que más escucho.
+          </Text>
+        </View>
+        <View style={styles.ImageContainer}>
+          <Image source={require('./assets/Album3.png')} style={styles.AlbumImage} />
+          <Text style={styles.TextAlbumContainer}>El tercero es f.e.a.r de Stand Atlantic. Es un álbum de rock experimental, mucho más movido,
+            pero a su vez con cierto nivel de equilibrio entre la música y el ritmo de la letra. Me atrevería a decir que, de los tres,
+            es el que menos escucho.
+          </Text>
+        </View>
+      </ScrollView>
+      <View style={styles.PaginationContainer}>
+        {[0, 1, 2].map((i) => (
+          <View
+            key={i}
+            style={[
+              styles.PaginationDot,
+              { opacity: currentIndex === i ? 1 : 0.3 }
+            ]}
+          />
+        ))}
       </View>
-    </SafeAreaView>
+    </View>
+  </SafeAreaView>
 
-  );
+);
 }
 
 const styles = StyleSheet.create({
--- Practica-#5/Practica5-InstaLike/App.js
+++ Practica-#5/Practica5-InstaLike/App.js
@@ -1,35 +1,74 @@
-import { StyleSheet, View, Image, Dimensions, SafeAreaView } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { StyleSheet, View, Image, Dimensions, ScrollView, TouchableOpacity, Text } from 'react-native';
+import { useState } from 'react';
 
 const { width } = Dimensions.get('window');
-const { height } = Dimensions.get('window');
 
 export default function App() {
+  const [numLike, setNumLike] = useState(322);
+  const [likeColor, setLikeColor] = useState('black');
+  const [guardado, setGuardado] = useState('black');
+
+  const handleLikesAndColor = () => {
+
+    if (likeColor === 'black') {
+      setNumLike(numLike + 1);
+      setLikeColor('red');
+    } else {
+      setNumLike(numLike - 1);
+      setLikeColor('black');
+    }
+  }
+
+  const handleGuardado = () => {
+    if (guardado === 'black') {
+      setGuardado('#0011ffff');
+    } else {
+      setGuardado('black');
+    }
+  }
+
+
   return (
-    <SafeAreaView style={styles.container}>
+    <ScrollView style={styles.container}>
 
       <View style={styles.header}>
         <Image
           source={require('./assets/instagram.png')}
           style={styles.logoImagen}
+
         />
       </View>
 
       <View style={styles.contenido}>
-        <Image source={require('./assets/User.png')} style={styles.User} />
-        <Image source={require('./assets/Pantallafotos.png')} style={styles.foto} />
-        <Image source={require('./assets/Likes.png')} style={styles.fotoLike} resizeMode="contain" />
-        <Image source={require('./assets/Comentarios.png')} style={styles.comentarios} resizeMode="contain" />
+        <Image source={require('./assets/Pantallafotos.png')} style={styles.Pantallafotos} />
+        <View style={styles.barraIconos}>
+          <View style={styles.barraIconosIzquierda}>
+            <TouchableOpacity onPress={handleLikesAndColor}>
+              <Ionicons name="heart-outline" size={28} color={likeColor} style={styles.icono} />
+            </TouchableOpacity>
+            <Text style={styles.numLike}>{numLike}</Text>
+            <Ionicons name="chatbubble-outline" size={24} color="black" style={styles.icono} />
+            <Ionicons name="paper-plane-outline" size={24} color="black" style={styles.icono} />
+          </View>
+          <View style={styles.barraIconosDerecha}>
+            <TouchableOpacity onPress={handleGuardado}>
+              <Ionicons name="bookmark-outline" size={24} color={guardado} style={styles.icono} />
+            </TouchableOpacity>
+          </View>
+        </View>
+
       </View>
 
       <View style={styles.menu}>
-        <Image
-          source={require('./assets/Menu.png')}
-          style={styles.menuImagen}
-          resizeMode="contain"
-        />
+        <Ionicons name="home" size={28} color="black" />
+        <Ionicons name="search" size={28} color="black" />
+        <Ionicons name="add-circle-outline" size={28} color="black" />
+        <Ionicons name="heart-outline" size={28} color="black" />
+        <Ionicons name="person-circle-outline" size={28} color="black" />
       </View>
 
-    </SafeAreaView>
+    </ScrollView>
   );
 }
 
@@ -40,10 +79,8 @@ const styles = StyleSheet.create({
   },
   header: {
     height: 60,
-    width: '100%',
     justifyContent: 'center',
     alignItems: 'center',
-    paddingLeft: 15,
     borderBottomWidth: 0.5,
     borderBottomColor: '#dbdbdb',
     marginTop: 30,
@@ -52,39 +89,44 @@ const styles = StyleSheet.create({
     width: 120,
     height: 40,
   },
-  User: {
-    width: width,
-    height: 60,
-  },
   contenido: {
     flex: 1,
-    justifyContent: 'flex-start',
   },
-  foto: {
-    marginTop: 30,
+  Pantallafotos: {
     width: width,
     height: width,
   },
-  fotoLike: {
-    width: width,
-    height: 100,
-    alignSelf: 'center',
+  barraIconos: {
+    flexDirection: 'row',
+    padding: 15,
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
-  comentarios: {
-    width: width,
-    height: 60,
-    alignSelf: 'center',
+  icono: {
+    marginRight: 15,
   },
   menu: {
     height: 70,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
     borderTopWidth: 0.5,
     borderTopColor: '#dbdbdb',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
   },
-  menuImagen: {
-    width: width,
-    height: 50,
+  barraIconosIzquierda: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '30%',
+    gap: 8,
+  },
+  barraIconosDerecha: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  numLike: {
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
